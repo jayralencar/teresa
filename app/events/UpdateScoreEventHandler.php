63b3d6c7c7b59a99efb7bbaf8b99816dc5c96268
Jayr Alencar <jayralencarpereira@gmail.com>
2017-04-23 11:54:17 -0300

    redis falha

---
+++ app/events/UpdateScoreEventHandler.php
@@ -0,0 +1,12 @@
+<?php
+class UpdateScoreEventHandler {
+ 
+    CONST EVENT = 'score.update';
+    CONST CHANNEL = 'score.update';
+ 
+    public function handle($data)
+    {
+        $redis = Redis::connection();
+        $redis->publish(self::CHANNEL, $data);
+    }
+}
